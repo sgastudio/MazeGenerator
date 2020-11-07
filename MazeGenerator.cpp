580ba3c8516c0e66d2f5abd017f884e3f7e886a4
Penguin <t.meng3@newcastle.ac.uk>
2020-11-08 04:13:06 +0800

    add playback menu item

--- MazeGenerator.cpp
+++ MazeGenerator.cpp
@@ -7,10 +7,11 @@
 using namespace std;
 
 enum MENU_SELECTIONS_E {
-	Generate = 1,
+	Generate = '1',
 	Load,
 	Save,
 	Preview,
+	PlayBack,
 	Credit,
 	Exit,
 };
@@ -20,23 +21,27 @@ int main(void)
 	Screen screenAgent;
 	Maze mazeAgent;
 	string fileAddr;
-	int input;
+	char input;
 	int exitCount;
 	int density;
 	int densityMax;
 	bool needOfPathfinding = false;
+	int playbackLoopTime=0;
 	Vector2 mazeSize;
 
 	screenAgent.SetWndTitle(TEXT("Maze Generator"));
 	screenAgent.SetMsg("Set to 8x8 Dot Font for better experience");
+
 	do {
 		screenAgent.Clear();
 		screenAgent.SetWndSize(63, 40);
 		//screenAgent.SetBufSize(65, 50);
 		screenAgent.Menu();
 		cin >> input;
-		screenAgent.Clear();
 
+
+
+		screenAgent.Clear();
 		switch (input)
 		{
 		case MENU_SELECTIONS_E::Generate:
@@ -46,69 +51,101 @@ int main(void)
 			exitCount = screenAgent.InputInt("How many exits do you want?\n\t(min=1 max=" + to_string((mazeSize.x + mazeSize.y) * 2 - 4) + ")");
 			Vector2::LimitInt(&exitCount, Vector2(1, (mazeSize.x + mazeSize.y) * 2 - 4));
 			densityMax = max(mazeSize.x, mazeSize.y) / 10;
-			density = screenAgent.InputInt("How dense it should be?\n\t(min=1 max=" + to_string(densityMax) + ")");
+			density = screenAgent.InputInt("How dense should it be?\n\t(min=1 max=" + to_string(densityMax) + ")");
 			Vector2::LimitInt(&density, Vector2(1, densityMax));
 			mazeAgent.Init(mazeSize, densityMax - density + 1, exitCount);
 			mazeAgent.Generate();
-			//mazeAgent.FindPath();
-			screenAgent.SetMsg("\033[32mMaze " + to_string(mazeSize.x) + "x" + to_string(mazeSize.y) + " " + to_string(density) + " level " + to_string(exitCount) + " exits generated.\033[0m");
+			screenAgent.SetMsgGood("Maze " + to_string(mazeSize.x) + "x" + to_string(mazeSize.y) + " " + to_string(density) + " level " + to_string(exitCount) + " exits generated.");
 			break;
+		
 		case MENU_SELECTIONS_E::Load:
 			screenAgent.PrintLogo();
 			fileAddr = screenAgent.InputString("Please the filename you want to load:");
 			if (fileAddr.empty() || fileAddr == "")
 			{
-				screenAgent.SetMsg("\033[31mFilename cannot be empty!\033[0m");
+				screenAgent.SetMsgWarn("Filename cannot be empty!");
 				break;
 			}
 			if (mazeAgent.LoadFromFile(fileAddr))
-				screenAgent.SetMsg("\033[32mMaze loaded.You can preview it now.\033[0m");
+				screenAgent.SetMsgGood("Maze loaded.You can preview it now.");
 			else
-				screenAgent.SetMsg("\033[33mMaze load fail.\033[0m");
+				screenAgent.SetMsgError("Maze load fail.");
 			break;
+		
 		case MENU_SELECTIONS_E::Save:
 			if (!mazeAgent.CheckGenerated())
 			{
-				screenAgent.SetMsg("\033[31mPlease generate/load the maze before save it!\033[0m");
+				screenAgent.SetMsgWarn("Please generate/load the maze before save it!");
 				break;
 			}
 			screenAgent.PrintLogo();
 			fileAddr = screenAgent.InputString("Please the filename you want to save:");
 			if (fileAddr.empty() || fileAddr == "")
 			{
-				screenAgent.SetMsg("\033[31mFilename cannot be empty!\033[0m");
+				screenAgent.SetMsgWarn("Filename cannot be empty!");
 				break;
 			}
 			if (mazeAgent.SaveToFile(fileAddr))
-				screenAgent.SetMsg("\033[32mMaze saved.\033[0m");
+				screenAgent.SetMsgGood("Maze saved.");
 			else
-				screenAgent.SetMsg("\033[33mMaze save fail.\033[0m");
+				screenAgent.SetMsgError("Maze save fail.");
 			break;
+		
 		case MENU_SELECTIONS_E::Preview:
 			if (!mazeAgent.CheckGenerated())
 			{
-				screenAgent.SetMsg("\033[31mPlease generate/load the maze before preview it!\033[0m");
+				screenAgent.SetMsgWarn("Please generate/load the maze before preview it!");
 				break;
 			}
 			screenAgent.PrintLogo();
 			needOfPathfinding = screenAgent.InputBool("Do you want to see the path to exit? \n\t(0 = No, 1 = Yes)");
+			mazeAgent.ClearPath();
 			if(needOfPathfinding)
 				mazeAgent.FindPath(screenAgent.InputInt("Which Kind of pathfinding method do you want? \n\t(0 = A* Algorithm, 1 = DFS Algorithm)"));
-			screenAgent.SetBufSize(mazeAgent.GetSize() + 1);
-			screenAgent.SetWndSize(mazeAgent.GetSize() + 1);
-			mazeAgent.Print(needOfPathfinding);
-			screenAgent.Pause();
+			screenAgent.SetBufSize(mazeAgent.GetSize() + 2);
+			screenAgent.SetWndSize(mazeAgent.GetSize() + 2);
+			mazeAgent.Print();
+			screenAgent.PauseEX("Press any key to continue");
 			break;
+		
+		case MENU_SELECTIONS_E::PlayBack:
+			if (!mazeAgent.CheckGenerated())
+			{
+				screenAgent.SetMsgWarn("Please generate/load the maze before playback!");
+				break;
+			}
+			screenAgent.PrintLogo();
+			playbackLoopTime = screenAgent.InputInt("How many mazes do you want to check? \n\t(at least 1)");
+			Vector2::LimitInt(&playbackLoopTime, Vector2(1, INT_MAX));
+			screenAgent.SetBufSize(mazeAgent.GetSize() + 3);
+			screenAgent.SetWndSize(mazeAgent.GetSize() + 3);
+			while (playbackLoopTime > 0)
+			{
+				mazeAgent.InitPlayers();
+				mazeAgent.FindPlayerPathAStar();
+				do
+				{
+					screenAgent.Clear();
+					mazeAgent.PrintWithPlayers();
+				} while (screenAgent.PauseEX("Press 'e' to exit\nother key to continue") != 'e' && mazeAgent.GetActivePlayerCount() > 0);
+				mazeAgent.Generate();
+				playbackLoopTime--;
+			}
+			break;
+		
 		case MENU_SELECTIONS_E::Credit:
 			screenAgent.PrintLogo();
 			screenAgent.PrintCredits();
 			screenAgent.Pause();
 			break;
+		
 		case MENU_SELECTIONS_E::Exit:
 			return 0;
 			break;
+		
 		default:
-			screenAgent.SetMsg("Invalid input");
+			screenAgent.SetMsgError("Invalid input");
+			input = 0;
 			break;
 		}
 	} while (true);
